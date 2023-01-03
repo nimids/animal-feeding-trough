c663b38605f8c7806e0b3e96b20fe2489dc759ef
nimids <80544703+nimids@users.noreply.github.com>
2023-01-03 00:17:48 -0800

    Update Gradle and Loom versions.
    Set Yarn and Fabric API versions to pull from Minecraft version.
    
    Make changes for Registry update.
    
    Make changes for Creative Inventory update.

--- src/main/java/slexom/vf/animal_feeding_trough/AnimalFeedingTroughMod.java
+++ src/main/java/slexom/vf/animal_feeding_trough/AnimalFeedingTroughMod.java
@@ -2,16 +2,20 @@ package slexom.vf.animal_feeding_trough;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import slexom.vf.animal_feeding_trough.block.FeedingTroughBlock;
@@ -30,10 +34,15 @@ public class AnimalFeedingTroughMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        FEEDING_TROUGH_SCREEN_HANDLER = Registry.register(Registry.SCREEN_HANDLER, REGISTRY_NAME, new ScreenHandlerType<>(FeedingTroughScreenHandler::new));
-        FEEDING_TROUGH_BLOCK = Registry.register(Registry.BLOCK, REGISTRY_NAME, new FeedingTroughBlock(AbstractBlock.Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
-        FEEDING_TROUGH_BLOCK_ITEM = Registry.register(Registry.ITEM, REGISTRY_NAME, new BlockItem(FEEDING_TROUGH_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
-        FEEDING_TROUGH_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, REGISTRY_NAME, BlockEntityType.Builder.create(FeedingTroughBlockEntity::new, FEEDING_TROUGH_BLOCK).build(null));
+        FEEDING_TROUGH_SCREEN_HANDLER = Registry.register(Registries.SCREEN_HANDLER, REGISTRY_NAME,
+                new ScreenHandlerType<>(FeedingTroughScreenHandler::new));
+        FEEDING_TROUGH_BLOCK = Registry.register(Registries.BLOCK, REGISTRY_NAME,
+                new FeedingTroughBlock(AbstractBlock.Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
+        FEEDING_TROUGH_BLOCK_ITEM = Registry.register(Registries.ITEM, REGISTRY_NAME,
+                new BlockItem(FEEDING_TROUGH_BLOCK, new Item.Settings()));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> content.add(FEEDING_TROUGH_BLOCK_ITEM));
+        FEEDING_TROUGH_BLOCK_ENTITY = Registry.register(Registries.BLOCK_ENTITY_TYPE, REGISTRY_NAME,
+                BlockEntityType.Builder.create(FeedingTroughBlockEntity::new, FEEDING_TROUGH_BLOCK).build(null));
         LOGGER.info("[Animal Feeding Trough] Load Complete! Enjoy :D");
     }
 
